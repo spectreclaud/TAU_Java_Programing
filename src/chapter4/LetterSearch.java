84ce395c8f25acb696a63d41f4a9f6a41eee414b
Rafał <spectreclaud@gmail.com>
2020-11-16 14:25:07 +0100

    TAU Java chapter4

--- src/chapter4/LetterSearch.java
+++ src/chapter4/LetterSearch.java
@@ -19,11 +19,11 @@ public class LetterSearch {
         boolean letterFound = false;
 
         //Search text for letter A
-        for(int i=0; i<text.length(); i++){
+        for(int i=0; !letterFound && i<text.length(); i++){
             char currentLetter = text.charAt(i);
             if(currentLetter == 'A' || currentLetter == 'a'){
                 letterFound = true;
-                break;
+                //break;
             }
         }
 
